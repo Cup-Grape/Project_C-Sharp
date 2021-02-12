09e98cade42b8085ac58fcf1075a544ed2b55adc
Cup-Grape <feliposs11@gmail.com>
2021-02-12 19:09:08 -0300

    Projeto 1 Maior ou Menor de idade

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P1code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano = 0;
+            Console.WriteLine("     Saber a idade     ");
+            Console.WriteLine("#########################");
+            Console.Write("Qual ano você nasceu? ");
+            ano = Convert.ToInt32(Console.ReadLine());
+            
+            int calc = 2021 - ano;
+
+            if (calc >= 18) 
+            {
+                Console.WriteLine("Você é maior de idade!");
+                Console.WriteLine($"Sua idade é {calc} anos");
+
+            }
+
+            else 
+            {
+                int calc2 = 18 - calc;
+                Console.WriteLine("Você é menor de idade!");
+                Console.WriteLine($"Volte daqui {calc2} anos!");
+
+            }
+
+                Console.WriteLine("#########################");
+            
+
+        }
+    }
+}
